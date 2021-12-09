ca5b1b52be34cfb1b81cc4ae0a56387ba1c8c09a
Tiago Leal <tiago.sleal12@gmail.com>
2021-12-08 21:07:40 -0300

    feat: create postfolio tests

---
+++ features/step_definitions/portfolio_steps.rb
@@ -0,0 +1,42 @@
+Dado "possui {int} portfólio(s) cadastrado(s)" do |count|
+    create_list(:portfolio, count, user_id: @user.id)
+  end
+  
+  
+  Quando "acessa a lista de portfólios" do
+    visit portfolios_path
+  end
+  
+  
+  Então "os {int} portfólios são listados" do |count|
+    expect(page).to have_css(".portfolio", count: count)
+  end
+  
+  
+  Então "nenhum portfólio é listado" do
+    expect(page).to have_css(".portfolio", count: 0)
+  end
+  
+  
+  Então "volta para a página de login" do
+    expect(page).to have_current_path("/users/sign_in")
+  end
+
+  Quando "clica para criar um novo portfólio" do
+    find(:xpath, "//a[@class='new-portfolio']").click
+  end
+  
+  
+  Então "o usuário deve ter {int} portfólios cadastrados" do |count|
+    expect(@user.portfolios.count).to eq count
+  end
+  
+  
+  Então "deve ver os dados iniciais do portfólio" do
+    expect(page).to have_field("portfolio[slug]", with: @user.portfolios.last.slug)
+  end
+  
+  
+  Então "é direcionado para a pagina de edição do portfólio" do
+    expect(page).to have_current_path(edit_portfolio_path(@user.portfolios.last))
+  end
