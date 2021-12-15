a4e872e98c0b7fce217abd1793b7c00c5d788f09
Tiago Leal <tiago.sleal12@gmail.com>
2021-12-14 23:55:35 -0300

    feat: create portfolio component

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -21,12 +21,12 @@ Rails.application.configure do
   # Show full error reports and disable caching.
   config.consider_all_requests_local       = true
   config.action_controller.perform_caching = false
-
+  
   # Raise exceptions instead of rendering exception templates.
   config.action_dispatch.show_exceptions = false
 
   # Disable request forgery protection in test environment.
-  config.action_controller.allow_forgery_protection = false
+  config.action_controller.allow_forgery_protection = true
 
   # Store uploaded files on the local file system in a temporary directory
   config.active_storage.service = :test
