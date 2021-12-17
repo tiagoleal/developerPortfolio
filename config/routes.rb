30b91b682fbb455b76ab8c6c0129b88bf6f65f1a
Tiago Leal <tiago.sleal12@gmail.com>
2021-12-16 21:52:07 -0300

    feat: created tests cucumber add and remove portfolio

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   root 'pages#home'
   devise_for :users
-  resources :portfolios, except: :new
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :portfolios, except: :new do
+    resources :tags, only: [:create, :destroy], param: :tag_id, controller: :portfolio_tags
+  end
+  resources :tags, only: :index
 end
