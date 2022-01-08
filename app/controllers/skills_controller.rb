b5074d32838baf76cd28b80f20edca1d9ae855ea
Tiago Leal <tiago.sleal12@gmail.com>
2022-01-08 10:38:43 -0300

    feat: set skill controller

--- app/controllers/skills_controller.rb
+++ app/controllers/skills_controller.rb
@@ -1,2 +1,24 @@
-class SkillsController < ApplicationController
+class SkillsController < PortfolioResourcesController
+ 
+  protected
+ 
+ 
+  def load_portfolio_resources
+    @portfolio.skills
+  end
+ 
+ 
+  def load_portfolio_resource
+    @portfolio.skills.find(params[:id])
+  end
+ 
+ 
+  def build_portfolio_resource
+    @portfolio.skills.build
+  end
+ 
+ 
+  def portfolio_resource_params
+    params.require(:skill).permit(:title, :percent)
+  end
 end
